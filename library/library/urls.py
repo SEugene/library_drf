7abc60b80514cb44b40b5a1b24cbde4988613e0e
SEugene <sea.05@list.ru>
2022-01-02 10:56:48 +0300

    Lesson 1. LibraryUser creared. Superuser created

--- library/library/urls.py
+++ library/library/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet
+from myusers.views import LibraryUserModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('libraryusers', LibraryUserModelViewSet)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
